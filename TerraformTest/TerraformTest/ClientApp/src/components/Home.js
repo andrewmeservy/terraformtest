5125b213ece223427acfbb1e5279e7723e7dd04f
Andrew Meservy <andrewmeservy@gmail.com>
2019-11-04 15:04:13 -0700

    downgrade to .net 2.2

--- TerraformTest/TerraformTest/ClientApp/src/components/Home.js
+++ TerraformTest/TerraformTest/ClientApp/src/components/Home.js
@@ -1,7 +1,7 @@
-import * as React from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 
-const Home = () => (
+const Home = props => (
   <div>
     <h1>Hello, world!</h1>
     <p>Welcome to your new single-page application, built with:</p>
--- TerraformTest/TerraformTest/ClientApp/src/index.js
+++ TerraformTest/TerraformTest/ClientApp/src/index.js
@@ -1,27 +1,29 @@
 import 'bootstrap/dist/css/bootstrap.css';
-
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'connected-react-router';
+import { ConnectedRouter } from 'react-router-redux';
 import { createBrowserHistory } from 'history';
 import configureStore from './store/configureStore';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 // Create browser history to use in the Redux store
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href') as string;
+const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const history = createBrowserHistory({ basename: baseUrl });
 
 // Get the application-wide store instance, prepopulating with state from the server where available.
-const store = configureStore(history);
+const initialState = window.initialReduxState;
+const store = configureStore(history, initialState);
+
+const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-    <Provider store={store}>
-        <ConnectedRouter history={history}>
-            <App />
-        </ConnectedRouter>
-    </Provider>,
-    document.getElementById('root'));
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
+  rootElement);
 
 registerServiceWorker();
